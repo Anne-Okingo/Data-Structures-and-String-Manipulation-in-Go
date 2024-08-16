93a317c940b89fbe11fb6ce38664a53f400a5031
a okingo <annekadiso@gmail.com>
2024-08-16 12:50:43 +0300

    Add : function that concanates slices

--- concatslice/main.go
+++ concatslice/main.go
@@ -4,10 +4,6 @@
 // Write a function ConcatSlice() that takes two slices of integers as arguments and returns the concatenation of the two slices.
 // Expected function
 
-// func ConcatSlice(slice1, slice2 []int) []int {
-
-// }
-
 // Usage
 
 // Here is a possible program to test your function:
@@ -36,34 +32,57 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
-	ConcatSlice([]int{1, 2, 3}, []int{4, 5, 6})
-	// fmt.Println(ConcatSlice([]int{}, []int{4, 5, 6, 7, 8, 9}))
-	// fmt.Println(ConcatSlice([]int{1, 2, 3}, []int{}))
+	fmt.Println(ConcatSlice([]int{1, 2, 3}, []int{4, 5, 6}))
+	fmt.Println(ConcatSlice([]int{}, []int{4, 5, 6, 7, 8, 9}))
+	fmt.Println(ConcatSlice([]int{1, 2, 3}, []int{}))
+	// fmt.Println(Atoi("254"))
 }
 
-func ConcatSlice(s1, s2 []int) {
-	result1 := ""
-for i := 0; i < len(s1); i++{
-		result1 += strconv.Itoa(s1[i]) + ", "
+func ConcatSlice(slice1, slice2 []int) []int {
+	return append(slice1, slice2...)
 }
 
-result2 := ""
-for j := 0; j < len(s2); j++{
-	if j != len(s2)-1{
-result2 += strconv.Itoa(s2[j]) + ","
-	}else{
-		result2 += strconv.Itoa(s2[j]) 
+// func ConcatSlice(s1, s2 []int) []int{
+// 	result1 := ""
+// 	// slice := []string{}
+// for i := 0; i < len(s1); i++{
+// 		result1 += strconv.Itoa(s1[i])
+// 	}
+
+// result2 := ""
+// for j := 0; j < len(s2); j++{
+// 	result2 += strconv.Itoa(s2[j])
+// }
+//  str := result1 + result2
+
+// num := []int{}
+// for i := 0; i < len(str); i++{
+// 	num = append(num, Atoi(string(str[i])))
+// }
+// return num
+
+// }
+
+func Atoi(s string) int {
+	multi := 1
+	result := 0
+	for i := range s {
+		if s[0] == '-' {
+			multi = -1
+			s = s[i:]
+			continue
+		}
+		if s[0] == '+' {
+			multi = 1
+			s = s[i:]
+			continue
+		}
 	}
+	for _, ch := range s {
+		result = result*10 + int(ch-'0')
+	}
+	return multi * result
 }
-str  := result1 + result2
-slice := []string{}
-for _, ch := range str{
-		slice = append(slice,string(ch))
-}
-fmt.Println(slice)
-}
-
