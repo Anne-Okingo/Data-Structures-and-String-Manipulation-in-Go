501a5f6fbddb8f230e2d247cfb49dffe788a73cf
aokingo <annekadiso@gmail.com>
2024-08-18 22:14:38 +0300

    Add : Firstrune fuction to return the first rune of a string

--- firstrune/main.go
+++ firstrune/main.go
@@ -1,31 +1,4 @@
 // Write a function that returns the first rune of a string.
-// package main
-
-// import "fmt"
-
-// func main() {
-// 	fmt.Println(FirstRune("Alice"))
-// }
-
-// func FirstRune(str string) string {
-// 	new := ""
-// 	for range str {
-// 		new = string(str[0])
-// 	}
-// 	return new
-// }
-// package main
-
-// import "fmt"
-
-// func main(){
-// 	fmt.Println(FirstRune("Hello!"))
-// }
-
-// func FirstRune(str string)rune{
-// 	s := []rune(str)
-// 	return (s[0])
-// }
 
 package main
 
@@ -40,7 +13,7 @@ func main() {
 
 func FirstRune(str string) rune {
 	s := []rune(str)
-	return (s[0])
+	return s[0]
 }
 
 
