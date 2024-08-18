02ca9cfc7da0f2e3bd83ef12efc0a9819beaea45
aokingo <annekadiso@gmail.com>
2024-08-18 21:49:32 +0300

    Implement function to find the first prime number greater than or equal to a given integer

---
+++ findprevprime/main.go
@@ -0,0 +1,75 @@
+// Write a function that returns the first prime number that is equal or superior to the int passed as parameter.
+
+// The function must be optimized in order to avoid time-outs with the tester.
+
+// (We consider that only positive numbers can be prime numbers)
+
+package main
+
+import (
+	"fmt"
+
+	// "golang.org/x/text/number"
+)
+
+// func IsPrime(nb int) bool {
+// 	if nb < 2 {
+// 		return false
+// 	}
+// 	for i := 2; i < nb; i++ {
+// 		if nb%i == 0 {
+// 			return false
+// 		}
+// 	}
+// 	return true
+// }
+
+// func IsPrime(nb int)bool{
+// 	if nb < 2{
+// 		return false
+// 	}
+// 	for i := 2; i < nb; i++{
+// 		if nb % i == 0{
+// 			return false
+// 		}
+// 	}
+// 	return true
+// }
+
+// func FindPrevPrime(num int) int {
+// 	if !IsPrime(num) {
+// 		num--
+// 	}
+//     return num
+// }
+
+func main() {
+	fmt.Println(FindPrevPrime(18))
+	fmt.Println(FindPrevPrime(5))
+	fmt.Println(FindPrevPrime(4))
+}
+
+func IsPrime(nb int)bool{
+	if nb < 2 {
+		return false
+	}
+	for i := 2; i < nb; i++{
+		if nb % i == 0{
+			return false
+		}
+	}
+	return true
+}
+
+func FindPrevPrime(num int)int{
+	if IsPrime(num){
+		return num
+	}else{
+		for i := num ; i > 0; i--{
+			if IsPrime(i){
+				return i
+			}
+		}
+	}
+	return num
+}
