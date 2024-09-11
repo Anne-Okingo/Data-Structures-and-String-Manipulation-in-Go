1ad22e956d626141f914f101e25d00383b0766bf
anne-okingo <you@example.com>
2024-09-11 22:59:36 +0300

    feat: add function that checks weather each words start with a capital letter or non numeric cahracters

--- Iscapitalized/main.go
+++ Iscapitalized/main.go
@@ -56,44 +56,15 @@ func main() {
 	fmt.Println(IsCapitalized(""))
 }
 
-// func IsCapitalized(s string) bool {
-// 	if len(s) == 0 {
-// 		return false
-// 	} else {
-// 		for i := 0; i < len(s); i++ {
-// 			if i == 0 && (s[i] >= 'a' && s[i]<= 'z'){
-// 				return false
-// 			}
-// 			if s[i] == ' ' && (s[i+1] >= 'a' && s[i+1]<= 'z') {
-// 				return false
-// 			}
-// 		}
-// 	}
-// 	return true
-// }
-
-// func IsCapitalized(s string) bool{
-// 	if len(s) == 0{
-// 		return false
-// 	}
-
-// 	for i := 0; i < len(s); i++{
-// 		if i == 0 && (s[i] >= 'a' && s[i] <= 'z'){
-// 			return false
-// 		}
-// 		if s[i] == ' ' && (s[i+1] >= 'a' && s[i+1] <= 'z') && i != len(s)-1{
-// 			return false
-// 		}
-// 	}
-// 	return true
-// }
-
-func IsCapitalized(s string) bool{
-	if len(s) == 0{
+func IsCapitalized(s string)bool{
+	if s == ""{
 		return false
 	}
-	for i, ch := range s{
-		if (ch >= 'a' && ch <= 'z') && i==0 || s[i] == ' ' && (s[i+1] >= 'a' && s[i+1] <= 'z'){
+	for i,ch := range s{
+		if i == 0 && ch >= 'a' && ch <= 'z'{
+			return false
+		}
+		if i < len(s)-1 && ch == ' ' && s[i+1] >= 'a' && s[i+1] <= 'z'{
 			return false
 		}
 	}
