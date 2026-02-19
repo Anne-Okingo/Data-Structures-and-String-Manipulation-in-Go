d1a1fcd5f1b429640453d423e9e8c3c0de6e2cb0
Anne-Okingo <annekadiso@gmail.com>
2026-02-19 14:08:46 +0300

    add countalpha

---
+++ countalpha/main.go
@@ -0,0 +1,41 @@
+// countalpha
+// Instructions
+// Write a function CountAlpha() that takes a string as an argument and returns the number of alphabetic characters in the string.
+
+// Expected functions
+// func CountAlpha(s string) int {
+
+// }
+// Usage
+// Here is a possible program to test your function:
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	fmt.Println(CountAlpha("Hello world"))
+// 	fmt.Println(CountAlpha("H e l l o"))
+// 	fmt.Println(CountAlpha("H1e2l3l4o"))
+// }
+// And its output:
+
+// $ go run .
+// 10
+// 5
+// 5
+
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.CountAlpha("Hello world"))
+	fmt.Println(piscine.CountAlpha("H e l l o"))
+	fmt.Println(piscine.CountAlpha("H1e2l3l4o"))
+}
